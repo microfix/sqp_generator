8f19d9065ff02be4e128246b283d7a4dccef1d6d
microfix <nicolaihlarsen@gmail.com>
2025-08-04 10:42:29 +0200

    Update vite.ts

--- server/vite.ts
+++ server/vite.ts
@@ -77,15 +77,4 @@ export function serveStatic(app: Express) {
   app.use("*", (_req, res) => {
     res.sendFile(path.resolve(distPath, "index.html"));
   });
-}    throw new Error(
-      `Could not find the build directory: ${distPath}, make sure to build the client first`,
-    );
-  }
-
-  app.use(express.static(distPath));
-
-  // fall through to index.html if the file doesn't exist
-  app.use("*", (_req, res) => {
-    res.sendFile(path.resolve(distPath, "index.html"));
-  });
 }
